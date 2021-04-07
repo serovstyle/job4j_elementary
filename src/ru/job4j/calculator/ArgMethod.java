58dc31d1fd652b89c8b5fcc2fbb80b7cf818226a
alexey.serov <serovstyle@gmail.com>
2021-04-07 09:11:16 +0300

    6. Идеальный вес [#511397] Added age output method.

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -4,11 +4,15 @@ public class ArgMethod {
     public static void hello(String name) {
         System.out.println("Hello, " + name);
     }
+    public static void printAge(int age){
+        System.out.println("U r " + age + " years old.");
+    }
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
 
         ArgMethod.hello(name);
+        ArgMethod.printAge(age);
     }
 }
